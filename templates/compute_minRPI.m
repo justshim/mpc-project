2f00d2dc548fc491f0a1b4a501c91857cb587b1b
justshim <58668136+justshim@users.noreply.github.com>
2022-05-14 14:07:30 +0200

    Still numerical tolerance issues but guess will have to add to the report

--- templates/compute_minRPI.m
+++ templates/compute_minRPI.m
@@ -7,5 +7,41 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function [H_tube,h_tube,n_iter] = compute_minRPI(K_tube,params)
-    % YOUR CODE HERE
+    % Task 28
+    % NOTE: Numerical tolerance issues on M1 Mac MATLAB 2021a
+    % Different behavior when running in debug vs run mode?
+    % Seems to pass tests when running on a different computer...
+    
+    % Get parameters
+    A = params.model.A;
+    B = params.model.B;
+    
+    H_w = params.constraints.DisturbanceMatrix;
+    h_w = params.constraints.DisturbanceRHS;
+    
+    A_k = A + (B*K_tube);
+    
+    % Initialize disturbance polytope
+    W = Polyhedron(H_w,h_w);
+        
+    % Initialize polytope
+    Phi_i = W;
+    
+    n_iter = 0;
+    
+    while true   
+        n_iter = n_iter + 1;
+        
+        % Minkowski Sum
+        Phi_j = plus(Phi_i, (A_k^n_iter) * W);
+                 
+        if eq(Phi_i.minHRep(), Phi_j.minHRep())
+            H_tube = Phi_j.A;
+            h_tube = Phi_j.b;
+            break;
+        end
+        
+        Phi_i = Phi_j;
+    end
+    
 end
