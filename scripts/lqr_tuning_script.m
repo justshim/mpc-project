8297448ea28c65a74b45bb00639eee777762bdb4
justshim <58668136+justshim@users.noreply.github.com>
2022-05-15 15:55:49 +0200

    Task 11

--- scripts/lqr_tuning_script.m
+++ scripts/lqr_tuning_script.m
@@ -32,20 +32,43 @@ close all
 my_params = generate_params();
 x0A = my_params.model.InitialConditionA;
 J_u_threshold = 11;
+numRounds = 3;
+N = 4;
+a = 0.5;
+b = 3;
 
-%% Parameter Generation
-% Sampling broadly in nx space (6D space)
-N = 10;
+x_log = logspace(-3,3,N);
 
-x_rand = logspace(-5,5,N);
-[q_1,q_2,q_3,q_4,q_5,q_6] = ndgrid(x_rand,x_rand,x_rand,x_rand,x_rand,x_rand);
+[q_1,q_2,q_3,q_4,q_5,q_6] = ndgrid(x_log,x_log,x_log,x_log,x_log,x_log);
 Q = [q_1(:),q_2(:),q_3(:),q_4(:),q_5(:),q_6(:)]';
 
 %% Tuning
-tic
-[tuning_struct, i_opt] = lqr_tuning(x0A,Q,my_params);
-toc
-q = Q(:,i_opt);
+% Sampling broadly in nx space (6D space)
+for i = 1:numRounds
+    [tuning_struct, i_opt] = lqr_tuning(x0A,Q,my_params);
 
-%% Save Results of Parameter Study
-% save('lqr_tuning_script', 'q', 'tuning_struct')
+    if ~isnan(i_opt) && tuning_struct(i_opt).InputCost <= J_u_threshold
+        q = Q(:,i_opt);
+        tuning_struct(i_opt).InputCost
+        
+        if i == numRounds
+            save('scripts/lqr_tuning_script', 'q', 'tuning_struct')
+            break;
+        end
+    else
+        disp("Did not identify feasible paramater vector Q");
+        break;
+    end
+
+    Q = zeros(6,N);
+
+    for j = 1:6
+        Q(j,:) = linspace(a*q(j),b*q(j),N);
+    end
+
+    [q_1,q_2,q_3,q_4,q_5,q_6] = ndgrid(Q(1,:),Q(2,:),Q(3,:),Q(4,:),Q(5,:),Q(6,:));
+    Q = [q_1(:),q_2(:),q_3(:),q_4(:),q_5(:),q_6(:)]';
+end
+
+disp('Tuning complete');
+fprintf('Input Cost = %.4f\n',tuning_struct(i_opt).InputCost);
